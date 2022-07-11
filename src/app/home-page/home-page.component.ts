b6262e7ddaae9aa87ef65fa7389e75d6091ae6e9
Andrew <andrew_shevchuk21@protonmail.com>
2022-07-11 17:34:39 +0300

    test

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Product} from "../shared/interface";
 import {ProductService} from "../shared/product.service";
 import {map, Observable, tap} from "rxjs";
+import {HomePageService} from "./home-page.service";
 
 @Component({
   selector: 'app-home-page',
@@ -16,11 +17,12 @@ export class HomePageComponent implements OnInit {
   count: number
   page: number
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService,
+              private paginServ: HomePageService) {
   }
 
   ngOnInit() {
-    this.page = 1;
+    this.page = this.paginServ.pageNum;
     this.product$ = this.productService.getAll({page: this.page}).pipe(tap(resp => {this.count = resp.count}), map(resp => resp.rows));
 
     // count =
@@ -30,5 +32,6 @@ export class HomePageComponent implements OnInit {
 
   onPageChange($event: any) {
     this.product$ = this.productService.getAll({page: $event.page + 1}).pipe(map(resp => resp.rows));
+    this.paginServ.pageNum = $event.page + 1;
   }
 }
