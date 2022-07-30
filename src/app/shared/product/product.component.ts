c96a22e20ce60654c1282ad5731b7beb24d5bd22
Andrew <andrew_shevchuk21@protonmail.com>
2022-07-30 11:45:54 +0300

    edit product component V2

--- src/app/shared/product/product.component.ts
+++ src/app/shared/product/product.component.ts
@@ -20,11 +20,27 @@ export class ProductComponent implements OnInit {
   @Input() product: Product;
   url = environment.apiURL;
   longOrders: LongOrder[];
+  longOrder: LongOrder;
+  cartR: boolean = false;
 
   constructor(public cartService: CartService) {}
 
   ngOnInit(): void {
     this.longOrders = this.cartService.longOrders$.value;
+    this.cartService.longOrders$.subscribe((longOrder: LongOrder[]) => {
+      longOrder.forEach((order) => {
+        if (order.product.id === this.product.id) {
+          this.longOrder = order;
+          this.cartR = true;
+        }
+      })
+    })
+    // this.cartService.longOrders$.value.forEach((order) => {
+    //   if (order.product.id === this.product.id) {
+    //     this.longOrder = order;
+    //     this.cartR = true;
+    //   }
+    // })
   }
 
   public addToCart(productId: number) {
